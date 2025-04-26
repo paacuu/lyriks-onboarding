e0f48afa8772fc050089a6d0846ea16badb0a9b9
Pascuzzi Romain <pascuzziromain@MacBook-Air-de-Pascuzzi.local>
2025-04-26 10:20:21 +0200

    Ajout correct du fb:app_id en propriété meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
     description: "Découvre comment closer avec impact en pilotant la clarté produit grâce à Lyriks & Facette.",
     images: [
       {
-        url: "https://lyriks-onboarding-sales.vercel.app/logo-og.png",
+        url: "https://lyriks-onboarding-sales.vercel.app/Logo2.png",
         width: 1200,
         height: 630,
         alt: "Logo Lyriks"
@@ -36,7 +36,7 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     title: "Lyriks Onboarding Sales",
     description: "Découvre comment closer avec impact en pilotant la clarté produit grâce à Lyriks & Facette.",
-    images: ["https://lyriks-onboarding-sales.vercel.app/logo-og.png"]
+    images: ["https://lyriks-onboarding-sales.vercel.app/Logo2.png"]
   },
   metadataBase: new URL("https://lyriks-onboarding-sales.vercel.app"),
   other: {
